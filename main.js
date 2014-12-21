2667951de78aec0c2481140b2542e7671112418e
DSFares <dsfares@gmail.com>
2014-12-20 17:27:55 -0800

    Add Bird (player)
    adding a bird to the game that can be controlled with the spacebar key to main.js

--- main.js
+++ main.js
@@ -4,19 +4,43 @@ var game = new Phaser.Game(400, 490, Phaser.AUTO, 'gameDiv');
 // Create our 'main' state that will contain the game
 var mainState = {
 
+  // This function will be executed at the beginning
+  // That's where we load the game's assets
   preload: function() {
-    // This function will be executed at the beginning
-    // That's where we load the game's assets
+    
+    // Change the background color of the game
+    game.stage.backgroundColor = '#71c5cf'';
+    
+    // Load the bird sprite
+    game.load.image('bird', 'assets/bird.png');
   },
   
+  // This function is called after the preload function
+  // Here we set up the game, display sprites, etc.  
   create: function() {
-    // This function is called after the preload function
-    // Here we set up the game, display sprites, etc.
+  
+    // Set the physics system
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+  
+    // Display the bird on the screen
+    this.bird = this.game.add.sprite(100, 245, 'bird');
+  
+    // Add gravity to the bird to make it fall
+    game.physics.arcade.enable(this.bird);
+    this.bird.body.gravity.y = 1000;
+    
+    // Call the 'jump' function when the spacekey is hit
+    var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    spaceKey.onDown.add(this.jump, this);
   },
   
+  // This function is called 60 times per second
+  // It contains the game's logic  
   update: function() {
-    // This function is called 60 times per second
-    // It contains the game's logic
+    
+    //If the bird is out of the world (too high or too low), call the 'restartGame' function
+    if (this.bird.inWorld == false)
+      this.retartGame();
   },
 };
 
